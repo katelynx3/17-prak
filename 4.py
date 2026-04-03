0ace2689a76e2d5d8169350b3aade3d8596f7718
katelyn_x3 <samson.ne.bob@gmail.com>
2026-04-03 14:15:32 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,1 +1,7 @@
-
+def is_password_good(password):
+    if len(password) < 8:
+        return False
+    else:
+        return True
+print(is_password_good('aabbCC11OP'))
+print(is_password_good('avC1pu'))
